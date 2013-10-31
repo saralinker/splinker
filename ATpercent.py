df2b3138d292b8eda0591c0e7ba608e4f8443996
saralinker <sara.linker@gmail.com>
2013-10-30 20:32:33 -0400

    Create ATpercent.py
    First full python script! 
    Calculates the AT% at each position of a 208 bp read 
    I realize there are a lot of issues with this script (ie: hard coding 220), but it's a place to start :D

---
+++ ATpercent.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+import sys
+import re
+
+inf = sys.argv[1]
+of = sys.argv[2]
+
+ih = open(inf,'r')
+oh = open(of,'w')
+
+try:
+    readcount = 0
+    carat = re.compile('>')
+    A = {}
+    T = {}
+    C = {}
+    G = {}
+    for it in range(1,220):
+        A[it] = 0
+        C[it] = 0
+        G[it] = 0
+        T[it] = 0
+    for line in ih:
+        line.strip
+        if carat.search(line):
+            count = 0
+            readcount += 1
+        else:
+            for i in line:
+                count += 1
+                if (i == "A"):
+                    tmp = A[count]
+                    tmp +=1
+                    A[count] = tmp
+                elif (i == "C"):
+                    tmp = C[count]
+                    tmp +=1
+                    C[count] = tmp
+                elif (i == "G"):
+                    tmp = G[count]
+                    tmp +=1
+                    G[count] = tmp
+                else:
+                    tmp = G[count]
+                    tmp +=1
+                    G[count] = tmp
+                
+finally:
+    for it in range(1,208):
+        ATtmp = ((A[it] + T[it]) * 1.0) / readcount
+        GCtmp = ((C[it] + G[it]) * 1.0) / readcount
+        tmpit = str(it)
+        ATtmp = str(ATtmp)
+        GCtmp = str(GCtmp)
+        oh.write(tmpit + "\t" + ATtmp + "\t" + GCtmp + "\n")
+        
+    ih.close
+    oh.close
+
